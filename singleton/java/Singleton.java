4d1a0d09581b75be99d24207b4060995c2e3d54d
Rafael Rendón Pablo <rafaelrendonpablo@gmail.com>
2016-03-14 22:34:58 -0600

    Add Singleton (anti?)pattern example

---
+++ singleton/java/Singleton.java
@@ -0,0 +1,20 @@
+public class Singleton {
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            synchronized (Singleton.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+
+    public void doAction() {
+        System.out.println("Action done!");
+    }
+}
