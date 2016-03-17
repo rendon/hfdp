230b0b316ae91f5293975185c724b0e90a1f50d4
Rafael Rendón Pablo <rafaelrendonpablo@gmail.com>
2016-03-17 07:58:56 -0600

    Add Command pattern example

---
+++ command/java/HottubOnCommand.java
@@ -0,0 +1,16 @@
+public class HottubOnCommand implements Command {
+
+    String location = "";
+
+    public Light(String location) {
+        this.location = location;
+    }
+
+    public void on() {
+        System.out.println(location + " light is on");
+    }
+
+    public void off() {
+        System.out.println(location + " light is off");
+    }
+}
---
+++ command/java/LivingroomLightOffCommand.java
@@ -0,0 +1,12 @@
+public class LivingroomLightOffCommand implements Command {
+    Light light;
+
+    public LivingroomLightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.off();
+    }
+}
+
